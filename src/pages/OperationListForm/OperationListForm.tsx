e4e2647cd1683c29c57b66105fead54a250d5bfb
Alexey Alexin <alexey.alexin@adonis.no>
2025-01-28 17:13:03 +0300

    Rename OperatinsForm

---
+++ src/pages/OperationListForm/OperationListForm.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import cn from 'clsx';
+import style from './OperationListForm.module.scss';
+import { OperationListProps, OperationListWithButton } from 'src/widgets/OperationList/OperationListWithButton';
+import { createRandomOperation } from 'src/entities/operation/Operation';
+
+const initOperations: OperationListProps = {
+  operations: [createRandomOperation(new Date().toISOString()), createRandomOperation(new Date().toISOString())],
+};
+
+export const OperationListForm: React.FC = () => {
+  return <OperationListWithButton {...initOperations} />;
+};
