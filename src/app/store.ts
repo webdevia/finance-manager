8aafe9eb3c7653ccee3df2af5cd95468832d4167
Lexin13 <lexintmp@gmail.com>
2025-02-10 03:56:46 +0300

    Clean up project

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,24 +3,16 @@ import { combineReducers } from 'redux';
 
 import appReducer from 'src/app/slices/appSlice';
 import authReducer from 'src/features/auth/authSlice';
-// import profileReducer from 'src/features/profile/profileSlice';
-import passwordReducer from 'src/features/profile/passwordSlice';
 import updatedOperationReducer from 'src/features/operation/updateOperation/slices/updatedOperationSlice';
-
-import { api } from '../features/auth/authApi';
 
 const rootReducer = combineReducers({
   auth: authReducer,
-  // profile: profileReducer,
-  password: passwordReducer,
   updatedOperation: updatedOperationReducer,
   app: appReducer,
-  api: api.reducer,
 });
 
 export const store = configureStore({
   reducer: rootReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(api.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
--- src/features/auth/authSlice.ts
+++ src/features/auth/authSlice.ts
@@ -1,7 +1,5 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 import { graphqlClient as client } from 'src/app/providers';
-import { SIGNIN_MUTATION } from './signIn/api/signIn.mutations';
-import { SIGNUP_MUTATION } from './signUp/api/signUp.mutations';
 import { ApolloError } from '@apollo/client';
 import { tokenStorage } from 'src/shared/storage/tokenStorage';
 
@@ -38,55 +36,12 @@ export const handleUnknownError = (serverError: string): AuthUserError => {
   return { fields: [], message: serverError };
 };
 
-export const signInUser = createAsyncThunk(
-  'auth/signInUser',
-  async ({ email, password }: AuthUser, { rejectWithValue }) => {
-    try {
-      const { data } = await client.mutate({
-        mutation: SIGNIN_MUTATION,
-        variables: { email, password },
-      });
-      return data.profile.signin.token;
-    } catch (err) {
-      if (err instanceof ApolloError) {
-        return rejectWithValue(handleAuthError(err));
-      }
-      return rejectWithValue(handleUnknownError('An unknown error occurred'));
-    }
-  }
-);
-
-export type SignUpUser = AuthUser & { commandId: string };
-
-export const signUpUser = createAsyncThunk(
-  'auth/signUpUser',
-  async ({ email, password, commandId }: SignUpUser, { rejectWithValue }) => {
-    try {
-      const { data } = await client.mutate({
-        mutation: SIGNUP_MUTATION,
-        variables: { email, password, commandId },
-      });
-
-      return data.profile.signup.token;
-    } catch (err) {
-      if (err instanceof ApolloError) {
-        return rejectWithValue(handleAuthError(err));
-      }
-      return rejectWithValue(handleUnknownError('An unknown error occurred'));
-    }
-  }
-);
-
 interface AuthState {
   token: string;
-  status: 'idle' | 'loading' | 'succeeded' | 'failed';
-  error: AuthUserError | null;
 }
 
 const initialState: AuthState = {
   token: tokenStorage.get(),
-  status: 'idle',
-  error: null,
 };
 
 const authSlice = createSlice({
@@ -95,48 +50,15 @@ const authSlice = createSlice({
   reducers: {
     signOut(state) {
       state.token = '';
-      state.status = 'idle';
       tokenStorage.remove();
       client.cache.reset();
-    },
-    resetError(state) {
-      state.error = null;
     },
     setToken(state, action) {
       tokenStorage.set(action.payload);
       state.token = action.payload;
     },
   },
-  extraReducers: (builder) => {
-    builder
-      // SignIn
-      .addCase(signInUser.pending, (state) => {
-        state.status = 'loading';
-      })
-      .addCase(signInUser.fulfilled, (state, action) => {
-        state.status = 'succeeded';
-        state.token = action.payload;
-        tokenStorage.set(action.payload);
-      })
-      .addCase(signInUser.rejected, (state, action) => {
-        state.status = 'failed';
-        state.error = action.payload as AuthUserError;
-      })
-      // SignUp
-      .addCase(signUpUser.pending, (state) => {
-        state.status = 'loading';
-      })
-      .addCase(signUpUser.fulfilled, (state, action) => {
-        state.status = 'succeeded';
-        state.token = action.payload;
-        tokenStorage.set(action.payload);
-      })
-      .addCase(signUpUser.rejected, (state, action) => {
-        state.status = 'failed';
-        state.error = action.payload as AuthUserError;
-      });
-  },
 });
 
-export const { signOut, resetError, setToken } = authSlice.actions;
+export const { signOut, setToken } = authSlice.actions;
 export default authSlice.reducer;
